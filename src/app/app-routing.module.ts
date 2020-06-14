a152749e7d4f336469d2317e9c7a8cc0dd6474d4
AlessPagl <63095800+AlessPagl@users.noreply.github.com>
2020-06-14 18:33:23 +0200

    Modifiche Pagina Profilo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrazioneComponent } from './registrazione/registrazione.component';
+import { PaginaProfiloComponent } from './pagina-profilo/pagina-profilo.component';
 import { PaginaNonTrovataComponent } from './pagina-non-trovata/pagina-non-trovata.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: "full" }, 
   { path: "login", component: LoginComponent },
   { path: "registrazione", component: RegistrazioneComponent },
+  { path: "pagina-profilo", component: PaginaProfiloComponent },
   { path: "**", component: PaginaNonTrovataComponent }
 ];
 
@@ -17,4 +19,5 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 export const routingComponents = [LoginComponent,
-                                  RegistrazioneComponent];
+                                  RegistrazioneComponent,
+                                  PaginaProfiloComponent];
