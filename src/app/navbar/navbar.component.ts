5170c603c1acdfa71a6b30d4fca8dec9737aac3d
AlessPagl <63095800+AlessPagl@users.noreply.github.com>
2020-06-15 21:48:15 +0200

    Navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { AngularFireAuth } from "@angular/fire/auth";
+import { HomeComponent } from '../home/home.component';
+import { LoginComponent } from '../login/login.component';
+import { RegistrazioneComponent } from '../registrazione/registrazione.component';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,7 +13,18 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private authService: AuthService, public afAuth: AngularFireAuth, public router: Router) { this.navbar(); }
+  /* constructor(private authService: AuthService, public afAuth: AngularFireAuth, public router: Router) { this.navbar(); } */
+
+  constructor(private router: Router) {
+    this.router.config.unshift(
+      { path: 'login', component: LoginComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'registrazione', component: RegistrazioneComponent },
+    );
+  }
+
+  links: Array<{ text: string, path: string }>;
+
 
   ngOnInit(): void {
   }
