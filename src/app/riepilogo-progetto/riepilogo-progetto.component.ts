1e5ee1bd44d1f9aa3384736961e03456b1f63066
AlessPagl <63095800+AlessPagl@users.noreply.github.com>
2020-06-22 18:19:43 +0200

    riepilogo-progetto modifiche

--- src/app/riepilogo-progetto/riepilogo-progetto.component.ts
+++ src/app/riepilogo-progetto/riepilogo-progetto.component.ts
@@ -1,8 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase';
+
+
+class progetto {
+
+  public nome;
+  public genere;
+  public num_partecipanti;
+  public descrizione;
+  public teamLeader;
+  public data_pubblicazione;
+  public num_teamMate;
+  public stato;
+
+  constructor(@Inject(String) nome, @Inject(String) genere, @Inject(String) num_partecipanti, @Inject(String) descrizione, @Inject(String) teamLeader, @Inject(Object) data_pubblicazione, @Inject(Boolean) num_teamMate, @Inject(Boolean) stato ) {
+    this.nome = nome;
+    this.genere = genere;
+    this.num_partecipanti = num_partecipanti;
+    this.descrizione = descrizione;
+    this.teamLeader = teamLeader;
+    this.data_pubblicazione = data_pubblicazione;
+    this.num_teamMate = num_teamMate;
+    this.stato = stato;
+
+  }
+
+}
 
 @Component({
   selector: 'app-riepilogo-progetto',
@@ -11,33 +37,53 @@ import * as firebase from 'firebase';
 })
 export class RiepilogoProgettoComponent implements OnInit {
 
-  public riepilogo ={AvanzamentoProgetto:"", data: new Date() };
+  public riepilogo = {AvanzamentoProgetto:"", data: new Date() };
+
+  public progetti: progetto[];
+
   public isDisabled = true;
   public tastoModifica = "Modifica";
   public dataRiepilogo = this.riepilogo.data.getDate() + "/" + (this.riepilogo.data.getMonth()+1) + "/" + this.riepilogo.data.getFullYear()
 
 
-  constructor(public firestore: AngularFirestore, public afAuth: AngularFireAuth, public router: Router) { }
+  constructor(public firestore: AngularFirestore, public afAuth: AngularFireAuth, public router: Router) { this.getProgetti()}
 
   ngOnInit(): void {  }
+  
+  getProgetti(){
+    this.afAuth.authState.subscribe(async (user)=>{
+      await this.firestore.collection("Progetto").ref.where("teamLeader", "==", user.uid).get().then((docs)=>{
+        docs.forEach(doc=>{
+          if (this.progetti===undefined)
+          {
+            this.progetti= [new progetto(doc.data().nome, doc.data().genere, doc.data().num_partecipanti, doc.data().descrizione, doc.data().teamLeader,doc.data().data_pubblicazione,doc.data().num_teamMate, doc.data().stato)]
+          }
+          else
+          {
+            this.progetti.push(new progetto(doc.data().nome, doc.data().genere, doc.data().num_partecipanti, doc.data().descrizione, doc.data().teamLeader, doc.data().data_pubblicazione,doc.data().num_teamMate, doc.data().stato))
+          }
+        })})
+      })
+  }
 
   async modificaValori() {
-    await this.afAuth.authState.subscribe((user) => {
-      
+
+    //subscribe((user) => {
+
       //il problema è qui, se salvi le informazioni con l'ID dell'utente non sai a che profilo
       //si riferisce
       //2 alternative: 
       //Recuperi e salvi le cose con l'ID del progetto
       //Salvi il riepilogo in un sub collection del progetto
 
-      this.firestore.collection("riepilogo").doc(user.uid).set({
-        ...this.riepilogo
+      // this.firestore.collection("riepilogo").doc(user.uid).set({
+      //   ...this.riepilogo
         
-      });
-
-    });
+      // });
 
   }
+
+
   modificaRiepilogo() {
     if (this.isDisabled === true) {
       this.isDisabled = false;
