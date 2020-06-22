4d0e45b3d625530420885491fb6eefd3adf5c6e1
AlessPagl <63095800+AlessPagl@users.noreply.github.com>
2020-06-22 16:47:07 +0200

    pagina-profilo.html

--- src/app/riepilogo-progetto/riepilogo-progetto.component.ts
+++ src/app/riepilogo-progetto/riepilogo-progetto.component.ts
@@ -11,9 +11,10 @@ import * as firebase from 'firebase';
 })
 export class RiepilogoProgettoComponent implements OnInit {
 
-  public riepilogo ={AvanzamentoProgetto:"", data: "" };
+  public riepilogo ={AvanzamentoProgetto:"", data: new Date() };
   public isDisabled = true;
   public tastoModifica = "Modifica";
+  public dataRiepilogo = this.riepilogo.data.getDate() + "/" + (this.riepilogo.data.getMonth()+1) + "/" + this.riepilogo.data.getFullYear()
 
 
   constructor(public firestore: AngularFirestore, public afAuth: AngularFireAuth, public router: Router) { }
@@ -21,19 +22,23 @@ export class RiepilogoProgettoComponent implements OnInit {
   ngOnInit(): void {  }
 
   async modificaValori() {
-
     await this.afAuth.authState.subscribe((user) => {
+      
+      //il problema è qui, se salvi le informazioni con l'ID dell'utente non sai a che profilo
+      //si riferisce
+      //2 alternative: 
+      //Recuperi e salvi le cose con l'ID del progetto
+      //Salvi il riepilogo in un sub collection del progetto
 
       this.firestore.collection("riepilogo").doc(user.uid).set({
-      
         ...this.riepilogo
+        
       });
 
     });
 
   }
   modificaRiepilogo() {
-    console.log("QUI")
     if (this.isDisabled === true) {
       this.isDisabled = false;
       this.tastoModifica = "Salva";
