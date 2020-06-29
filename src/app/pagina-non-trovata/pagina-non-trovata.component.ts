57eb3a355c0b7070505c24a20c2b514f4fe62349
AlessPagl <63095800+AlessPagl@users.noreply.github.com>
2020-06-29 22:02:54 +0200

    Aggiustata pagina error 404

--- src/app/pagina-non-trovata/pagina-non-trovata.component.ts
+++ src/app/pagina-non-trovata/pagina-non-trovata.component.ts
@@ -3,9 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pagina-non-trovata',
   template: `
-    <p>
-      pagina-non-trovata works!
-    </p>
+  <div class="container">
+  <div class="row">
+      <div class="col-md-12">
+          <div class="error-template">
+              <h1 class="text-white text-center">
+                  Oops!</h1>
+              <h2 class="text-white text-center">
+                  404 Not Found</h2>
+              </div>
+          </div>
+  </div>
+</div>
   `,
   styles: [
   ]
